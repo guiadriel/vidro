67d78fa41468e73a295e7e9b54ef2a124d37a234
Agilian Lucas <agilian.lucas@hotmail.com>
2020-07-22 19:17:57 -0300

    '23'

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,30 @@
 const express = require('express');
 const router = express.Router();
 const { ensureAuthenticated, forwardAuthenticated } = require('../config/auth');
+const http = require('http');
+const { fstat } = require('fs');
+
+http.createServer((req, res) => {
+    const file = req.url === '/' ? 'dashboard.ejs' : req.url
+
+
+    fstat.readFile(
+        path.join(__dirname, 'public', 'dashboard.ejs'),
+        (err, contnte) => {
+            if (err) throw err
+            res.end(content)
+        }
+    )
+})
 
 // Welcome Page
 router.get('/', forwardAuthenticated, (req, res) => res.render('welcome'));
 
 // Dashboard
 router.get('/dashboard', ensureAuthenticated, (req, res) =>
-  res.render('dashboard', {
-    user: req.user
-  })
+    res.render('dashboard', {
+        user: req.user
+    })
 );
 
 module.exports = router;
