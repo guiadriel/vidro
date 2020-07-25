7e314e8087beafda6f6f895e591237d8ed206255
Agilian Lucas <agilian.lucas@hotmail.com>
2020-07-25 19:37:52 -0300

    'w178tu'

--- public/js/a.js
+++ public/js/a.js
@@ -840,7 +840,7 @@ function inserirLinhaTabelaFabrica() {
                 divx.setAttribute("style", "padding-left:60px; padding-top:15px");
                 var texto = document.getElementById('largura').value;
                 var txt_aux = texto.split();
-                divx.appendChild(document.createTextNode(parseFloat(txt_aux[0]) - descontoLargura));
+                divx.appendChild(document.createTextNode((parseFloat(txt_aux[0]) - descontoLargura)).toString());
                 newCell.appendChild(divx);
                 break;
             case 3:
