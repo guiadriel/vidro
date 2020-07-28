aa5dac184b3c402c0e6adc82d3d5e956b0cef887
Agilian Lucas <agilian.lucas@hotmail.com>
2020-07-28 19:15:09 -0300

    'c91'

--- public/js/a.js
+++ public/js/a.js
@@ -327,9 +327,10 @@ function inserir() {
     inserirLinhaTabela();
     inserirLinhaTabelaFabrica();
     soma123 += parseFloat(document.getElementById("Total").value);
+    soma123 += parseFloat(document.getElementById("maoDeObra").value)
     soma123Fabrica += parseFloat(document.getElementById("Total").value);
     document.getElementById("soma1234").innerHTML = number_format(soma123, 2, ",", ".");
-    var vista = (parseFloat(soma123) - ((5 * parseFloat(soma123)) / 100)) + parseFloat(document.getElementById("maoDeObra").value);
+    var vista = (parseFloat(soma123) - ((5 * parseFloat(soma123)) / 100));
     console.log("Mao de obra adicionada: " + document.getElementById("maoDeObra").value);
     console.log(vista);
     document.getElementById("vista").innerHTML = number_format(vista, 2, ",", ".");
