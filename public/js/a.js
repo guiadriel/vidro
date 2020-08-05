c78ca3a2ec8b6cd6f90483b30557a65a45842a2f
Agilian Lucas <agilian.lucas@hotmail.com>
2020-08-05 00:08:51 -0300

    'c252'

--- public/js/a.js
+++ public/js/a.js
@@ -27,7 +27,7 @@ $("btfabrica").click(function() {
 });
 
 function whatsApp() {
-    window.open("https://wa.me/+05519997208913?text=https://www.calculesuaobra.com.br/teste.pdf", "minhaJanela", "height=800px,width=800px");
+    window.open("https://wa.me/+05519997208913?text=Oi%20*FULANO*!%20Estou%20te%20enviando%20o%20PDF%20do%20orçamento%20conforme%20combinamos,%20continuo%20a%20disposição.%20https://www.calculesuaobra.com.br/teste.pdf", "minhaJanela", "height=800px,width=800px");
 }
 
 function imprimir() {
