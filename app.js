7b804177255d0bf9f02f4fc706938cc71a3bd98b
AgilianPC <agilian.lucas@hotmail.com>
2020-09-06 22:41:18 -0300

    upload  multer3

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(expressLayouts);
 app.set('view engine', 'ejs');
 
 app.post('/', upload.single('img'), (req, res) =>{
-    console.log(req.body, req.files)
+    console.log(req.body, req.file)
     res.send('ok')
 })
 
